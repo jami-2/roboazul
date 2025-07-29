84b7664ffa6a53e583b51708c6cc1baa63926495
Mircas001 <79532341+Mircas001@users.noreply.github.com>
2025-07-29 19:07:29 -0300

    outro: Adicionar comentários

--- roboazul.ino
+++ roboazul.ino
@@ -6,6 +6,12 @@
 #include <NewPing.h>
 #include <QTRSensors.h>
 
+// Modificações:
+// Trocar GY-521 para conexão direta
+// Instalar QTR no chão, entre as rodas
+// Mover os sensores de cor para trás
+
+
 // Configurações de hardware
 #define TRIGGER_PIN  48
 #define ECHO_PIN     49
